ad1a13050121eccc203032010f5c8eb7a03532bd
imanapriya <148778714+imanapriya@users.noreply.github.com>
2026-03-01 19:51:18 +0530

    Time: 0 ms (100.00%) | Memory: 24 MB (19.70%) - LeetSync

--- 283-move-zeroes/move-zeroes.cpp
+++ 283-move-zeroes/move-zeroes.cpp
@@ -1,16 +1,12 @@
 class Solution {
 public:
     void moveZeroes(vector<int>& nums) {
-        int insertpos = 0;
-
-        for (int i = 0; i < nums.size(); i++) {
-            if (nums[i] != 0) {
-                nums[insertpos++] = nums[i];
+        int nonzero=0;
+        for(int j=0;j<nums.size();j++){
+            if(nums[j]!=0){
+                swap(nums[nonzero],nums[j]);
+                nonzero++;
             }
-        }
-
-        for (int i = insertpos; i < nums.size(); i++) {
-            nums[i] = 0;
         }
     }
 };
